ce9a4a714aad3acb168e45b17d37aaf5c1da3e1c
Joshua van der Geize <mrmuffincake232@gmail.com>
2022-03-23 15:14:49 +0100

    Update controller.vhd

--- controller.vhd
+++ controller.vhd
@@ -25,4 +25,43 @@ entity controller is
 
 end entity;
 
---architecture struct of controller is
+architecture struct of controller is
+	type controller_state is (	forward,
+					slight_left,
+					sharp_left,
+					slight_right,
+					sharp_right,
+					stop 		);
+
+	signal state, new_state		: controller_state;
+
+begin
+	process (clk)
+	begin
+		if(rising_edge (clk)) then
+			if (reset = '1') then
+				state 	<= stop;
+			else
+				state	<= new_state;
+			end if;
+		end if;
+	end process;
+
+
+	process ( clk, state )
+	begin
+		case state is
+			when forward 	=>
+				dir_left	<= '1';
+				dir_right	<= '0';
+				rst_left	<= '0';
+				rst_right	<= '0';
+
+			when slight_left =>
+				dir_right	<= '0';
+				rst_right	<= '0';
+				rst_left	<= '1';
+
+
+
+					
